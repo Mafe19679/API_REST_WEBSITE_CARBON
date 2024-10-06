ed500d2033f02f534fcbe8bdcdec4734bad39821
davricardo <davricardo@hotmail.com>
2024-10-06 17:53:25 -0500

    Actualizar conexion.js
    Solicita Conexión con API

---
+++ JS/conexion.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const apiKey = 'KEY'; //
+const apiUrl = 'https://www.websitecarbon.com/api/
+
+axios.get(apiUrl, {
+    headers: {
+        'Authorization': `Bearer ${apiKey}`
+    }
+})
+.then(response => {
+    const data = response.data;
+    // Procesar los datos aquí
+    console.log(data);
+})
+.catch(error => {
+    console.error('Error al obtener datos:', error);
+});
